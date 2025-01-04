0950311951126118cf0446e8b627ee1395fd8455
Ed Calvert <72658447+EdwardCalvert@users.noreply.github.com>
2025-01-04 08:51:01 +0000

    Style apple status bar

--- app/root.tsx
+++ app/root.tsx
@@ -32,6 +32,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="manifest" href="manifest.json" />
         <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent"></meta>
+        <meta name="theme-color" content="#C39424"/>
         <Meta />
         <Links />
       </head>
