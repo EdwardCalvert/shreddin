0a1f4959102dba678334fdaf0344e642e4d6d42b
Ed Calvert <72658447+EdwardCalvert@users.noreply.github.com>
2025-01-04 10:06:22 +0000

    Fix transparent header on ios

--- app/root.tsx
+++ app/root.tsx
@@ -31,7 +31,6 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="manifest" href="manifest.json" />
-        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent"></meta>
         <meta name="theme-color" content="#C39424"/>
         <Meta />
         <Links />
